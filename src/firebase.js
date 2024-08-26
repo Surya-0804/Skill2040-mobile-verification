62b5052f1b497ec039baeb9bbb92eaedd566d386
Surya-0804 <surya.abothula@gmail.com>
2024-08-26 20:54:40 +0530

    small changes

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = firebase.initializeApp(firebaseConfig);
